2ebef4031a2d087b915386f1b609266d37113603
core <yalagin@gmail.com>
2023-04-04 15:47:35 +0700

    first

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className={'logo'}>
+                <h1>Coddemik</h1>
+            </div>
+            <div className={'menu'}>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </div>
+            <div className={'phone'}>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
