f22cefc1f4a80bd64175c15ed919b294dbce7d3d
core <yalagin@gmail.com>
2023-04-05 21:29:22 +0700

    add feedback

---
+++ components/miniCard/MiniCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './MiniCard.module.scss';
+import Image from "next/image";
+
+const MiniCard = ({emoji, text, title}) => {
+    return (
+        <div className={styles.container}>
+            <Image src={emoji} alt={'Emoji'} height={30} width={30}/>
+            <h4>{title}</h4>
+            <p>{text}</p>
+        </div>
+    );
+};
+
+export default MiniCard;
--- components/prosComponent/ProsComponent.jsx
+++ components/prosComponent/ProsComponent.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import styles from './ProsComponent.module.scss'
 import Image from "next/image";
+import MiniCard from "@/components/miniCard/MiniCard";
+import Sticker from "@/components/sticker/Sticker";
+
 const ProsComponent = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Lorem ipsum.</h1>
-            <div className={styles.left}>
-                <Image src={'/video/boy-lesson.jpg'}
-                       alt={'Image'}
-                       width={500} height={300}
-                       className={styles.image}
-                />
+            <div className={styles.wrapper}>
+                <div className={styles.left}>
+                    <Image src={'/video/boy-lesson.jpg'}
+                           alt={'Image'}
+                           width={500} height={300}
+                           className={styles.image}
+                    />
+                </div>
+                <div className={styles.right}>
+                    <MiniCard
+                        title={'Lorem ipsum.'}
+                        emoji={'/image/nerd.png'}
+                        text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, reiciendis?'}
+                    />
+                    <MiniCard
+                        title={'Lorem ipsum.'}
+                        emoji={'/image/nerd.png'}
+                        text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, reiciendis?'}
+                    />
+                    <MiniCard
+                        title={'Lorem ipsum.'}
+                        emoji={'/image/nerd.png'}
+                        text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, reiciendis?'}
+                    />
+                </div>
             </div>
-            <div className={styles.right}>
-
-            </div>
+            <Sticker text={'Lorem ipsum dolor sit amet.'}
+                     sticker={'/image/nerd.png'}
+                     top={'80%'}
+                     right={'1%'}
+            />
+            <Image src={'/shapes/nature.svg'}
+                   alt={'Shape'}
+                   width={60}
+                   height={60}
+                   className={styles.nature}
+            />
         </div>
     );
 };
